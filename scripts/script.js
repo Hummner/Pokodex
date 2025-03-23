a356488a4e0d7165307417705c11520ccace5890
Hummner <bence.cservenyak@gmail.com>
2025-03-23 09:05:19 +0100

    basestat in progress

--- scripts/script.js
+++ scripts/script.js
@@ -8,17 +8,25 @@ console.log(pokemons);
 
 function init() {
     getPokemons();
-    
-
 }
 
-
-
-function renderPokemons(pokemonQuantity) {
+function renderPokemons() {
     let contentRef = document.getElementById("content");
+    let morePokemonBtn = document.getElementById("more_pokemon_btn")
     contentRef.innerHTML = "";
 
-    for (let indexPokemon = 0; indexPokemon < pokemonQuantity; indexPokemon++) {
+    for (let indexPokemon = 0; indexPokemon < 20; indexPokemon++) {
+
+        contentRef.innerHTML += getSmallBoxTemplate(indexPokemon);
+        renderTypes(indexPokemon);
+    }
+    morePokemonBtn.innerHTML = `<button onclick="renderMorePokemons()" class="more-button">More Pokemon</button>`;
+}
+
+function renderMorePokemons() {
+    let contentRef = document.getElementById("content");
+
+    for (let indexPokemon = 20; indexPokemon < 40; indexPokemon++) {
 
         contentRef.innerHTML += getSmallBoxTemplate(indexPokemon);
         renderTypes(indexPokemon);
@@ -33,10 +41,7 @@ function renderTypes(indexPokemon) {
         typesRef.innerHTML += getTypesTemplate(indexPokemon, indexType);
         renderBackgroundColor(indexPokemon, indexType)
     }
-
-    
 }
-
 
 function renderBackgroundColor(indexPokemon, indexType) {
     let smallBoxRef = document.getElementById("small_box_" + indexPokemon)
@@ -57,7 +62,7 @@ function renderBackgroundColor(indexPokemon, indexType) {
 
 async function getPokemons() {
     
-    for (let indexID = 1; indexID < 21; indexID++) {
+    for (let indexID = 1; indexID < 41; indexID++) {
          let pokemonData = await loadData(indexID);
          pokemons.push(pokemonData);
     }
@@ -67,9 +72,45 @@ async function getPokemons() {
 }
 
  async function loadData(path="") {
-    let response = await fetch(BASE_URL + "/" + path)
+    let response = await fetch(BASE_URL + "/" + path);
     let responseJSON =  await response.json();
-    return responseJSON
+    return responseJSON;
+}
 
+
+function showDialog(indexPokemon) {
+    let overlayRef = document.getElementById("overlay")
+    overlayRef.classList.toggle("d_none");
+    dialogNavbar(indexPokemon)
+    
+}
+
+function dialogNavbar(indexPokemon) {
+    let dialogNavbarRef = document.getElementById("overlay_nav");
+    dialogNavbarRef.innerHTML = getDialogNavbarTemplate(indexPokemon);
+    showAbout(indexPokemon)
+} 
+
+function showAbout(indexPokemon) {
+    let detailsRef = document.getElementById("details");
+    detailsRef.innerHTML = getShowAboutTemplate(indexPokemon);
+    showAboutAbilities(indexPokemon);
+}
+
+function showAboutAbilities(indexPokemon) {
+    let abilitiesRef = document.getElementById("abilities_" + indexPokemon);
+    let abilitiesArray = [];
    
+    for (let indexAbillity = 0; indexAbillity < pokemons[indexPokemon].abilities.length; indexAbillity++) {
+        abilitiesArray.push(pokemons[indexPokemon].abilities[indexAbillity].ability.name);
+    }
+    const capitalize = abilitiesArray.map((item) => {
+        return item[0].toUpperCase() + item.slice(1).toLocaleLowerCase();
+    });
+    abilitiesRef.innerHTML = capitalize.join(", ");
+}
+
+
+function showBasestats(indexPokemon) {
+    
 }
--- scripts/template.js
+++ scripts/template.js
@@ -1,7 +1,7 @@
 function getSmallBoxTemplate(indexPokemon) {
-    return `<div id="small_box_${indexPokemon}" class="small-box">
+    return `<div id="small_box_${indexPokemon}" class="small-box" onclick="showDialog(${indexPokemon})">
                     <div id="id_number_${indexPokemon}" class="id-number">#${pokemons[indexPokemon].id}</div>
-                    <div class="name">${pokemons[indexPokemon].name}</div>
+                    <div class="name uppercase">${pokemons[indexPokemon].name}</div>
                     <div class="pokemon-type">
                         <div class="types" id="types_${indexPokemon}">
                         </div>
@@ -12,7 +12,37 @@ function getSmallBoxTemplate(indexPokemon) {
 
 
 function getTypesTemplate(indexPokemon, indexType) {
-    return `<div><span>${pokemons[indexPokemon].types[indexType].type.name}</span></div>`
+    return `<div class="uppercase"><span class="uppercase">${pokemons[indexPokemon].types[indexType].type.name}</span></div>`
 }
 
 
+function getDialogNavbarTemplate(indexPokemon) {
+    return `<span onclick="showAbout(${indexPokemon})">About</span>
+            <span onclick="showBasestats(${indexPokemon})>Base stats</span>
+            <span>Gender</span>
+            <span>Shiny</span>`
+}
+
+
+function getShowAboutTemplate(indexPokemon) {
+    pokemons[indexPokemon].weight = pokemons[indexPokemon].weight / 10;
+    pokemons[indexPokemon].height = pokemons[indexPokemon].height / 10;
+    return `<table>
+                <tr>
+                    <td>Name:</td>
+                    <td class="uppercase">${pokemons[indexPokemon].name}</td>
+                </tr>
+                <tr>
+                    <td>Height:</td>
+                    <td class="uppercase">${pokemons[indexPokemon].height} meter</td>
+                </tr>
+                <tr>
+                    <td>Weight:</td>
+                    <td class="uppercase">${pokemons[indexPokemon].weight} kg</td>
+                </tr>
+                <tr>
+                    <td>Abilities:</td>
+                    <td class="uppercase" id="abilities_${indexPokemon}"></td>
+                </tr>
+            </table>`
+}
